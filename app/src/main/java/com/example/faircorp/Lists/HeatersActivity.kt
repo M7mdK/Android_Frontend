42f9b49b9bfdb1bae48b94b215d3c19939970352
M7mdK <mohammad.kassem.1999@hotmail.com>
2022-01-11 08:11:12 +0100

    heaters

--- app/src/main/java/com/example/faircorp/Lists/HeatersActivity.kt
+++ app/src/main/java/com/example/faircorp/Lists/HeatersActivity.kt
@@ -4,9 +4,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.faircorp.R
 
-class HeatersActivity : AppCompatActivity() {
+import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.faircorp.APIs.ApiServices
+import com.example.faircorp.Adapters.HeatersAdapter
+import com.example.faircorp.BasicActivity
+import com.example.faircorp.Entities.HeaterActivity
+import com.example.faircorp.HEATER_NAME_PARAM
+import com.example.faircorp.Listeners.OnHeaterSelectedListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class HeatersActivity : BasicActivity(), OnHeaterSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heaters)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list_heaters)
+        val adapter = HeatersAdapter(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        lifecycleScope.launch(context = Dispatchers.IO) {
+            runCatching { ApiServices().heaterApiService.findAll().execute() }
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) {
+                        adapter.update(it.body() ?: emptyList())
+                    }
+                }
+                .onFailure {
+                    withContext(context = Dispatchers.Main) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on heaters loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+    override fun onHeaterSelected(id: Long) {
+        val intent = Intent(this, HeaterActivity::class.java).putExtra(HEATER_NAME_PARAM, id)
+        startActivity(intent)
     }
 }
